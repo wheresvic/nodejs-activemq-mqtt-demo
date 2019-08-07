0154bfef32d770d8bc23a58e34f274ddf6d83656
Victor Parmar <vic@smalldata.tech>
2019-08-07 21:45:49 +0200

    F initial subscription

---
+++ src/nodejs-activemq-mqtt-demo.js
@@ -0,0 +1,65 @@
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://localhost:1883", { clientId: "nodejs-activemq-mqtt-demo", clean: false });
+
+client.on("connect", () => {
+  client.subscribe({ "home/test": { qos: 2 } }, function(err, granted) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(granted);
+  });
+});
+
+client.on("message", (topic, message) => {
+  console.log("received message %s %s", topic, message);
+});
+
+/**
+ * Handle the different ways the application can shutdown
+ */
+function handleAppExit(options, err) {
+  if (err) {
+    console.error(err);
+  }
+
+  if (options.cleanup) {
+    console.log("cleaning up");
+  }
+
+  if (options.exit) {
+    process.exit();
+  }
+}
+
+/*
+process.on(
+  "exit",
+  handleAppExit.bind(null, {
+    cleanup: true
+  })
+);
+*/
+
+process.on(
+  "SIGINT",
+  handleAppExit.bind(null, {
+    cleanup: true,
+    exit: true
+  })
+);
+
+process.on(
+  "SIGTERM",
+  handleAppExit.bind(null, {
+    cleanup: true,
+    exit: true
+  })
+);
+
+process.on(
+  "uncaughtException",
+  handleAppExit.bind(null, {
+    exit: true
+  })
+);
